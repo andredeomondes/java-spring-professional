6d0b6176e47a458e6466312938d7301fe9969335
Andre Deomondes <andredeomondes.dev@gmail.com>
2026-01-09 19:01:50 -0300

    COMPONENTES DE INJEÇÃO DE DEPENDÊNCIA - Passando o projeto para Spring

---
+++ a-componentes-de-Injecao-de-dependencia/a02-salary-java-spring/aula/src/main/java/com/andredeomondes/aula/aula/AulaApplication.java
@@ -0,0 +1,30 @@
+package com.andredeomondes.aula.aula;
+
+import com.andredeomondes.aula.entities.Employee;
+import com.andredeomondes.aula.services.SalaryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+@ComponentScan({"com.andredeomondes"})
+public class AulaApplication implements CommandLineRunner {
+
+	@Autowired
+	private SalaryService salaryService;
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Employee employee = new Employee("Maria", 4000.0);
+		System.out.println(salaryService.netSalary(employee));
+	}
+}
---
+++ a-componentes-de-Injecao-de-dependencia/a02-salary-java-spring/aula/src/test/java/com/andredeomondes/aula/AulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andredeomondes.aula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
